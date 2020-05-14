6a9be4ba5e5b93389e697d2ab6acb557631c38f7
Mikaela <>
2020-05-14 14:24:31 +1000

    skeleton for while loop with validation, currently infinite loop

--- src/main/java/org/mikaela/App.java
+++ src/main/java/org/mikaela/App.java
@@ -1,13 +1,37 @@
 package org.mikaela;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+
+        String username = "omar";
+        String password = "snoopy12";
+
+
+        boolean login = false;
+
+        while (login) {
+            login = validation(username, password);
+        }
     }
+
+    private static boolean validation(String username, String password) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter username: ");
+        String userInput = scanner.next();
+        System.out.println("Enter password: ");
+        String userPass = scanner.next();
+
+        if ((userInput.equals(username)) && (userPass.equals(password))) {
+            return true;
+
+        } else {
+            return false;
+        }
+    }
+
 }
+
